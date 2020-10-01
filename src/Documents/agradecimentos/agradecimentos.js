1b078c209b7eec85e4c0a8225073d32dc73e43d9
montsouz <guilhermesouzamont@gmail.com>
2020-10-01 20:57:56 -0300

    feat: add stories and read more

---
+++ src/Documents/agradecimentos/agradecimentos.js
@@ -0,0 +1,36 @@
+export const AGRADECIMENTOS = [
+    {
+        text: `Na minha memória, a República Jardim de Alah é a referência pessoal em Vila Rica de Nossa Senhora do Pilar de Ouro Preto, tanto quanto também o é, a Escola de Minas de Ouro Preto, nas velhas Minas Gerais.
+        Aqui se formam grandes profissionais e também grandes amizades, e suas famílias são irmanadas.
+        Famílias menores como as filhas da consanguinidade de cada um de nós, e famílias maiores como as irmandades, que florescem do cotidiano, na comunhão de vivências e sonhos, num mundo normal, onde cada um de nós é ele mesmo, e se encaixa numa rede mental, qual peça chave, imprescindível, num grande tabuleiro, na faina da juventude e da vida, com seus requintes de realidade.
+        À maneira das miríades, base sólida na profundidade oceânica, magnífico arcabouço das maravilhosas ilhas de corais, sustentando a vida marinha em seus fundamentos, a vivência do cotidiano constrói as irmandades, e nos torna irmãos.
+        A velha amizade verdadeira, a estima costumeira, a imensa saudade de um tempo bom vivido intensamente, e a alegria do reencontro.
+        Irmãos e amigos da Jardim de Alah, é com imenso carinho que me dirijo a cada um de vocês, trazendo no coração um indescritível amálgama dos meus melhores sentimentos, para comemorarmos juntos, os meus 40 anos de graduação acadêmica.
+        `,
+        author: 'Edson Pereira Bueno “Papola”',
+    },
+    {
+        text: `Cheguei em Ouro Preto em Fevereiro de 1980 para cursar Engenharia Metalurgica. Sempre foi um sonho meu estudar na Escola de Minas.  Ao ser aprovado no vestibular, não tive dúvidas em ir para Ouro Preto. Ao chegar na cidade, comecei a enfrentar a “Batalha das Repúblicas” e tive a sorte de ser escolhido na Jardim de Alah. Começou aí um período de minha vida de muito aprendizado, companheirismo, amizades e dedicação aos estudos. Aliado aos momentos acadêmicos, diante das temidas cadeiras de cálculos, sempre sobrava tempo para um bonde e rodadas de caixeta na República. Nos fins de semana, era sagrado ligar o som no talo e tomar o que tinha disponivel para as investidas insólidas no CAEM. Ficavamos também na expectativa da visita de um bondoso ex-aluno para pagar umas cervejas na Praça e no encontro de uma namorada para dar uns beijos.
+        Enfretamos diversas greves, movimentos e assembleias estudantis no período de 1980 a 1985, o que me levou a perda de um semestre. Participei da Diretoria do CAEM no período de 1983/84, onde tive a oportunidade da minha primeira experiencia gerencial. 
+        Em Julho/85 me formei e fui admitido na antiga Belgo Mineira em João Monlevade, onde permaneci por 35 anos até me aposentar. Neste período tive a oportundiade de fazer Mestrado e Doutorado na UFMG e de atuar como professor em uma escola de engenharia nesta cidade.
+        Hoje tenho orgulho das amizades que construi no convivio da gloriosa República durante meu período de moradia e de novas amizades que a todo ano se renovam e multiplicam. O aprendizado do convivio me guia para toda a vida. Sou enternamente grato a esta egrégia casa que nos inspira união, tradição e irmandade. Alha é Pai!`,
+        author: 'Helder Carvalho Ferreira “Varal”',
+    },
+    {
+        text:
+            '25 anos de muito agradecimento: agradecimento aos meus pais, a minha família, à República Jardim de Alah, à Escola de Minas, à cidade de Ouro Preto, a todos os professores da Universidade, aos meus contemporâneos, aos ex-alunos e aos alunos que hoje cuidam da Jardim! Enfim, o meu sincero agradecimento a todos que participaram e que me ajudaram nessa caminhada! Engenheiro Metalurgista',
+        author: 'Arilton Café de Moura “Xabão”',
+    },
+    {
+        text: `Meus momentos na Jardim de Alah, com um breve texto não como explicar.
+        Muitos sonhos tive quando cheguei lá, expectativas e euforias também.
+        Lá aprendi que meu tempo de menino havia ficado para trás. Que minhas angustias de adolescente de nada serviam naquele lugar.
+        Cheguei porta adentro com a ambição de um novo ser, aberto a uma nova vida. Com grandes amigos e muito conhecimento absorver. Um momento em que virei gente.
+        Momento esse que aprendi a respeitar e a ser respeitado, diálogo foi o que aprendi lá. 
+        Nas famosas Reuniões, com Ata e poder de votação, as decisões da maioria reinam e ensinam. Ah, como ensinam...
+        Nos pequenos 15 anos de formado posso, sem receio de errar, dizer que o meu maior aprendizado foi lá, dentro da República Jardim de Alah.
+         Lá se aprende o que de mais complexo há no mundo, o trato com as pessoas
+        `,
+        author: 'Antônio O. S. Sobrinho “Kamizinha”',
+    },
+];
